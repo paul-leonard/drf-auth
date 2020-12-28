2622db6da51ea8a20ea3b6ebfeda98de6d8d8ba3
paul-leonard <paul.ryan.leonard@gmail.com>
2020-12-28 00:18:24 -0800

    permissions require login to view

--- daily_entries/views.py
+++ daily_entries/views.py
@@ -1,9 +1,13 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Entry
 from .serializers import EntrySerializer
 
 # Create your views here.
 
 class EntryListCreate(ListCreateAPIView):
+  queryset = Entry.objects.all()
+  serializer_class = EntrySerializer
+
+class EntryRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
   queryset = Entry.objects.all()
   serializer_class = EntrySerializer
