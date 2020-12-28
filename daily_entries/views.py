c8c1b7e0629b39c1c5127edc6bb5f7e8c853c3f0
paul-leonard <paul.ryan.leonard@gmail.com>
2020-12-28 00:29:14 -0800

    only the author of the journal entry is allowed to modify/delete

--- daily_entries/views.py
+++ daily_entries/views.py
@@ -1,6 +1,7 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Entry
 from .serializers import EntrySerializer
+from .permissions import IsAuthorOrReadOnly
 
 # Create your views here.
 
@@ -11,3 +12,4 @@ class EntryListCreate(ListCreateAPIView):
 class EntryRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
   queryset = Entry.objects.all()
   serializer_class = EntrySerializer
+  permission_classes = (IsAuthorOrReadOnly,)
